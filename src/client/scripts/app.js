1222af86303d2d96433bd12020473882b3495d56
Griffin Byron <puginabox@gmail.com>
2015-03-12 16:19:13 -0400

    fixing position of navigation

--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -1,25 +1,25 @@
-jQuery(document).ready(function($){
-$('body').plusAnchor({
-easing: 'easeInOutExpo',
-offsetTop: -20,
-speed: 1000,
-onInit: function( base ) {
+jQuery(document).ready(function ($) {
+    $('body').plusAnchor({
+        easing: 'easeInOutExpo',
+        offsetTop: -20,
+        speed: 1000,
+        onInit: function (base) {
 
-if ( base.initHash != '' && $(base.initHash).length > 0 ) {
-window.location.hash = 'hash_' + base.initHash.substring(1);
-window.scrollTo(0, 0);
+            if (base.initHash != '' && $(base.initHash).length > 0) {
+                window.location.hash = 'hash_' + base.initHash.substring(1);
+                window.scrollTo(0, 0);
 
-$(window).load( function() {
+                $(window).load(function () {
 
-timer = setTimeout(function() {
-$(base.scrollEl).animate({
-scrollTop: $( base.initHash ).offset().top
-}, base.options.speed, base.options.easing);
-}, 2000); // setTimeout
+                    timer = setTimeout(function () {
+                        $(base.scrollEl).animate({
+                            scrollTop: $(base.initHash).offset().top
+                        }, base.options.speed, base.options.easing);
+                    }, 2000); // setTimeout
 
-}); // window.load
-}; // if window.location.hash
+                }); // window.load
+            }; // if window.location.hash
 
-} // onInit
-});
+        }
+    });
 });
