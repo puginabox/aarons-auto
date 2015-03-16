55b2afc45806eb9d5996a0d344a24351095ddec3
Griffin Byron <puginabox@gmail.com>
2015-03-16 19:38:21 -0400

    perfecting map formatting

--- src/client/scripts/locations.js
+++ src/client/scripts/locations.js
@@ -4,7 +4,7 @@
      var center = new google.maps.LatLng(38.885421, -77.176552);
 
      var mapOptions = {
-         zoom: 11,
+         zoom: 9,
          center: center,
          mapTypeId: google.maps.MapTypeId.ROADMAP
      };
@@ -15,19 +15,19 @@
      var marker1 = new google.maps.Marker({
          position: new google.maps.LatLng(38.885421, -77.176552),
          map: map,
-         title: "Herndon Shop"
+         title: "Herndon Shop -  hours: 9am to 5pm weekedays"
      });
 //---- Location 2   ----/
      var marker2 = new google.maps.Marker({
          position: new google.maps.LatLng(38.861365, -77.083511),
          map: map,
-         title: "Arlington Megacomplex"
+         title: "Arlington Megacomplex -  hours: 9am to 5pm weekends"
      });
 //---- Location 3   ----/
      var marker3 = new google.maps.Marker({
          position: new google.maps.LatLng(38.933041, -77.161274),
          map: map,
-         title: "McLean Main Offices"
+         title: "McLean Main Offices -  hours: 9am to 5pm weekedays"
      });
 
 
