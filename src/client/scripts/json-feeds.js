aaa62ca06ed0cdb4c321a578f91a4462b2f0ed2b
Griffin Byron <puginabox@gmail.com>
2015-03-17 08:02:12 -0400

    adding brands

--- src/client/scripts/json-feeds.js
+++
@@ -1,8 +0,0 @@
-$.getJSON('/scripts/testimonials.json', function (data) {
-    var output = '<ul>';
-    $.each(data, function (key, val) {
-        output += '<li>' + val.name + '</li>';
-    });
-    output += '</ul>';
-    $('.testen').html(output);
-}); //get JSON
